20795b92021c8dea946bea1021bf3aa5a50eb456
kuntal <bhattacharjeekuntal01@gmail.com>
2026-01-09 21:35:14 +0530

    Add button on webpage

--- content.js
+++ content.js
@@ -1,7 +1,30 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const bookmarkImageURL =  chrome.runtime.getURL("assets/bookmark.png");
+
+function addBookmarkButton(){
+    const bookmarkButton = document.createElement("img");
+    bookmarkButton.setAttribute('id', 'add-bookmark-button');
+    bookmarkButton.src = bookmarkImageURL;
+    bookmarkButton.alt = 'bookmark problem';
+    
+    const style = document.createElement("style");
+    style.textContent = `
+    #add-bookmark-button {
+        height: 30px,
+        width: 30px,
+        cursor: pointer
+        transition: filter 0.2s ease;
+    }
+    #add-bookmark-button:hover {
+        filter:
+        drop-shadow(0 0 6px #ffd700)
+        drop-shadow(0 0 12px #ffd700);
+    }
+    `;
+    document.head.appendChild(style);
+
+    const problemDesc = document.getElementsByClassName('coding_runBtn__JDrPy')[0];
+    problemDesc.insertAdjacentElement("beforebegin", bookmarkButton);
+
+    bookmarkButton.addEventListener("click", () => alert(`Problem Bookmarked!`));
+}
+window.addEventListener("load", addBookmarkButton);
